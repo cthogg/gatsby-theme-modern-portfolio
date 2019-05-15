29b6acd1517a97544d58c22d86f4ba46b9dc32ca
Chris Hogg <chogg50@gmail.com>
2019-05-15 11:25:03 +0200

    feat: add email logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 import React from "react"
 import SEO from "../components/seo"
-import { FaLinkedin } from 'react-icons/fa'
+import { IoLogoLinkedin, IoMdMail } from 'react-icons/io'
+
 import "../components/layout.css"
 
 const LINKEDIN_URL = 'https://www.linkedin.com/in/christopher-hogg-081a41128/'
-
+const EMAIL_ADDRESS = 'chrishogg@protonmail.com'
 const IndexPage = () => (
   <div className='wrapper'>
     <SEO title="Home" keywords={[`web developer`]} />
     <div className='two'>
       <h1>I am Chris Hogg.</h1>
       <h1>Web Developer.</h1>
-      <a href="mailto:chrishogg@protonmail.com">chrishogg@protonmail.com</a>
-      <p> <a href={LINKEDIN_URL}> <FaLinkedin></FaLinkedin> </a> </p>
+      <p> <a href={`mailto:${EMAIL_ADDRESS}`}> <IoMdMail></IoMdMail> </a>   <a href={LINKEDIN_URL}> <IoLogoLinkedin></IoLogoLinkedin> </a> </p>
     </div>
   </div >
 )
